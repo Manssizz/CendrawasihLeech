e8c29a86bb56f1f10bc0283d172da6fb288d7e61
Manssizz <fajarmanssizz@yahoo.com>
2021-05-30 15:54:49 +0700

    fix unzip command

--- tobrot/helper_funcs/create_compressed_archive.py
+++ tobrot/helper_funcs/create_compressed_archive.py
@@ -8,8 +8,6 @@ import os
 import shutil
 import subprocess
 
-from tobrot import LEECH_UNZIP_COMMAND, LOGGER
-
 
 async def create_archive(input_directory):
     return_name = None
@@ -57,7 +55,7 @@ async def unzip_me(input_directory):
         # uncompressed_file_name = os.path.splitext(base_dir_name)[0]
         uncompressed_file_name = get_base_name(base_dir_name)
         LOGGER.info(uncompressed_file_name)
-        g_cmd = [LEECH_UNZIP_COMMAND, f"{input_directory}"]
+        g_cmd = ["./extract", f"{input_directory}"]
         ga_utam = await asyncio.create_subprocess_exec(
             *g_cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
