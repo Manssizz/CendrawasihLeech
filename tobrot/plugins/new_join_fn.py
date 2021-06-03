b855cd6521b39cddeaf72bf5537d9cd6541e5068
manssizz <fajarmanssizz@gmail.com>
2021-06-03 21:27:46 +0700

    add greeting messages

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -25,5 +25,30 @@ async def help_message_f(client, message):
     # display the /help
 
     await message.reply_text(
-        """""",
+        """
+        Available Commands:
+        /leech - Leech torrent file or direct link file and upload to Telegram.
+        /clone - Clone Drive file or folder to your Drive.
+        /sync - Leech torrent/direct link to Drive.
+        /tgsync - This will mirror the telegram files to ur Drive.
+        /log - This will send you a txt file of the logs.
+        /status - Get download/upload process status. 
+        /stop - Stop single process command 
+        /tarleech - Leech torrent file or direct link file and upload to Telegram and upload as archive file.
+        /tarsync - This command will compress the folder/file and will upload to Drive.
+        /leechuntar - This will unarchive file and upload to telegram.
+        /untarsync - This will unarchive file and upload to Drive.
+        /tguntar - This will unarchive telegram file and upload to cloud.
+        /ytleech - Single video Youtube downloader to Telegram.
+        /ytdlsync - Single video Youtube downloader to Drive.
+        /getsize - Get size folder in Drive.
+        /renewme - Cancel all download process.
+        /pytleech - Youtube downloader and send video/music to telegram.
+        /pytdlsync - Youtube downloader and send video/music to Drive.
+        /upload - Hmmm... wait
+        /ytdl - This will download and upload to your Drive.
+        /ytpl - This download youtube playlist and upload to your Drive.
+        /rclone - This will change your drive config on fly.(First one will be default)
+        /rename - To rename the telegram files.
+        """,
         disable_web_page_preview=True)
