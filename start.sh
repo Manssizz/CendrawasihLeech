7a4d3a1bc23a24209494595368d780c1ba3e4d26
manssizz <fajarmanssizz@gmail.com>
2021-06-08 23:23:49 +0700

    change dir

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [[ -n $RCLONE_CONFIG ]]; then
  echo "Rclone config detected"
- echo -e "$RCLONE_CONFIG" > /app/rclone.conf
+ echo -e "$RCLONE_CONFIG" > /CendrawasihLeech/rclone.conf
 fi
 # Create homepage web page
 # python3 homepage.py &
