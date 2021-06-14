f3ca8806dbe94d898f41d2fff7674ce676e69598
manssizz <fajarmanssizz@gmail.com>
2021-06-14 15:45:43 +0700

    try add support detecting torrent file in end of url

--- tobrot/helper_funcs/extract_link_from_message.py
+++ tobrot/helper_funcs/extract_link_from_message.py
@@ -37,8 +37,7 @@ async def extract_link(message, type_o_request):
     
     elif message.text is not None:
         if message.text.lower().endswith(".torrent"):
-            url = await message.download()
-            custom_file_name = message.caption
+            url = extract_url_from_entity(message.entities, message.text)
 
         elif "|" in message.text:
             url_parts = message.text.split("|")
