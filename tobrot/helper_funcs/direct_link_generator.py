1c9857664fcf7185df8d7167c750568bcfa03b94
manssizz <fajarmanssizz@gmail.com>
2021-07-19 16:34:36 +0700

    update fembed scripts

--- tobrot/helper_funcs/direct_link_generator.py
+++ tobrot/helper_funcs/direct_link_generator.py
@@ -21,7 +21,7 @@ import lk21
 import requests, cfscrape
 from bs4 import BeautifulSoup
 from js2py import EvalJs
-# from lk21.extractors.bypasser import Bypass
+from lk21.extractors.bypasser import Bypass
 from base64 import standard_b64encode
 
 from tobrot.helper_funcs.exceptions import DirectDownloadLinkException
