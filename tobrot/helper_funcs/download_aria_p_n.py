f2d03b06643ccd65af59b7e6c3f093f9566ad47a
manssizz <fajarmanssizz@gmail.com>
2021-07-21 16:00:30 +0700

    enable custom file name

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -170,10 +170,10 @@ def add_torrent(aria_instance, torrent_file_path):
 
 def add_url(aria_instance, text_url, c_file_name):
     options = None
-    # if c_file_name is not None:
-    #     options = {
-    #         "dir": c_file_name
-    #     }
+    if c_file_name is not None:
+        options = {
+            "dir": c_file_name
+        }
     if "zippyshare.com" in text_url \
         or "osdn.net" in text_url \
         or "mediafire.com" in text_url \
