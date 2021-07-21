7a20d7f43be4a8afc01716b71c7177a2b3037b26
manssizz <fajarmanssizz@gmail.com>
2021-07-21 16:27:12 +0700

    disable custom folder name

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -169,7 +169,7 @@ def add_torrent(aria_instance, torrent_file_path):
 
 
 def add_url(aria_instance, text_url, c_file_name):
-    # options = None
+    options = None
     # if c_file_name is not None:
     #     options = {
     #         "dir": c_file_name
