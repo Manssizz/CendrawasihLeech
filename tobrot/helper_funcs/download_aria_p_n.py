f18f1de4179306d8e987f94d9c5cb6c8c2b700c9
Manssizz <fajarmanssizz@yahoo.com>
2021-08-03 21:21:52 +0700

    add mirror site fembed and sbembed domain

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -284,6 +284,7 @@ async def call_apropriate_function(
     await check_progress_for_dl(
         aria_instance, err_message, sent_message_to_update_tg_p, None
     )
+
     # if incoming_link.startswith("magnet:"):
     #     #
     #     err_message = await check_metadata(aria_instance, err_message)
@@ -293,9 +294,9 @@ async def call_apropriate_function(
     #         await check_progress_for_dl(
     #             aria_instance, err_message, sent_message_to_update_tg_p, None
     #         )
-    #     else:
-    #         return False, "Can't getting metadata \n\n#MetaDataError"
-    # await asyncio.sleep(1)
+    # else:
+    #     return False, "Can't getting metadata \n\n#MetaDataError"
+    await asyncio.sleep(1)
     file = aria_instance.get_download(err_message)
     to_upload_file = file.name
     com_g = file.is_complete
