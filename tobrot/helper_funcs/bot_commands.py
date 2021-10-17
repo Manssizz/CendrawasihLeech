34ee3fb5ca0d29cba1e336bec7b430dd76636710
Manssizz <fajarmanssizz@yahoo.com>
2021-10-17 16:46:22 +0700

    add bot command

---
+++ tobrot/helper_funcs/bot_commands.py
@@ -0,0 +1,17 @@
+class _BotCommands:
+    def __init__(self):
+        self.LeechCommand = 'leech'
+        self.ExtractCommand = 'extract'
+        self.ArchiveCommand = 'archive'
+        self.RenameCommand = 'rename'
+        self.StatusCommand = 'status'
+        self.SpeedCommand = 'speedtest'
+        self.YtdlCommand = 'ytdl'
+        self.PytdlCommand = 'pytleech'
+        self.HelpCommand = 'help'
+        self.LogCommand = 'log'
+        self.MediaInfoCommand = 'mediainfo'
+        self.TsHelpCommand = 'tshelp'
+
+
+BotCommands = _BotCommands()
