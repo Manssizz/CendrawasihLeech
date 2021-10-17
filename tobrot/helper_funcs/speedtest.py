37c4a5345cae622d1b3435fc4d67e727651a0fca
Manssizz <fajarmanssizz@yahoo.com>
2021-10-17 15:51:16 +0700

    re-enable speedtest

--- tobrot/helper_funcs/speedtest.py
+++ tobrot/helper_funcs/speedtest.py
@@ -4,6 +4,7 @@ from ..functions.Human_Format import human_readable_bytes
 
 torlog = logging.getLogger(__name__)
 
+
 async def get_speed(message):
     imspd = await message.reply("`Running speedtest...`")
     test = Speedtest()
@@ -14,7 +15,7 @@ async def get_speed(message):
     result = test.results.dict()
     path = (result['share'])
     string_speed = f'''
-**Speedtest Result:-**
+**Speedtest Result:**
 Server Name: `{result["server"]["name"]}`
 Country: `{result["server"]["country"]}, {result["server"]["cc"]}`
 Sponsor: `{result["server"]["sponsor"]}`
@@ -25,4 +26,5 @@ ISP: `{result["client"]["isp"]}`
 '''
     await imspd.delete()
     await message.reply(string_speed, parse_mode="markdown")
-    torlog.info(f'Server Speed result:-\nDL: {human_readable_bytes(result["download"] / 8)}/s UL: {human_readable_bytes(result["upload"] / 8)}/s')
+    torlog.info(
+        f'Server Speed result:-\nDL: {human_readable_bytes(result["download"] / 8)}/s UL: {human_readable_bytes(result["upload"] / 8)}/s')
